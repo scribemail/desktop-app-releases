85ba4e9c8aac25e177e1684f504b4ceafa0c4e15
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-07 23:10:02 +0200

    Fix

--- build/after-all-artifact-build.js
+++ build/after-all-artifact-build.js
@@ -1,17 +1,18 @@
 const { exec } = require("child_process");
 const find = require("lodash/find");
+const path = require("path");
 
 exports.default = async function returnInTuneFiles(context) {
-  const { appOutDir, packager, electronPlatformName } = context;
-  const appVersion = packager.appInfo.productFilename;
+  const { appOutDir, artifactPaths } = context;
 
-  const pkgPath = find(context.artifactPaths, (localPath) => localPath.includes(".pkg"));
+  const pkgPath = find(artifactPaths, (localPath) => localPath.includes(".pkg"));
   const exePath = find(context.artifactPaths, (localPath) => localPath.includes("Setup") && !localPath.includes("blockmap"));
+  const exeFileName = path.basename(exePath);
 
   console.log("pkgPath", pkgPath);
   console.log("exePath", exePath);
 
-  if (electronPlatformName === "darwin") {
+  if (pkgPath) {
     const command = `${appOutDir.replace("dist", "build")}/IntuneAppUtil -c ${pkgPath} -o ${appOutDir}`;
     console.log("mac command", command);
     await exec(command);
@@ -23,8 +24,8 @@ exports.default = async function returnInTuneFiles(context) {
   await exec(`mkdir ${appOutDir}/intune-source`);
   console.log("win command 2", `copy /B /Y ${exePath} ${exePath.replace("dist", "dist/intune-source")}`);
   await exec(`copy /B /Y ${exePath} ${exePath.replace("dist", "dist/intune-source")}`);
-  console.log("win command 3", `${appOutDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${appOutDir}/intune-source" -s "${appOutDir}/Scribe Setup ${appVersion}.exe" -o ${appOutDir}`);
-  await exec(`${appOutDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${appOutDir}/intune-source" -s "${appOutDir}/Scribe Setup ${appVersion}.exe" -o ${appOutDir}`);
+  console.log("win command 3", `${appOutDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${appOutDir}/intune-source" -s "${exeFileName}" -o ${appOutDir}`);
+  await exec(`${appOutDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${appOutDir}/intune-source" -s "${exeFileName}" -o ${appOutDir}`);
 
   return [`${appOutDir}/${exePath.gsub(".exe", "")}.intunemac`];
 };
