a71ed5b908c92586a4b99bb1e06c9968e2b38251
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-07 22:58:31 +0200

    Update

--- build/after-all-artifact-build.js
+++ build/after-all-artifact-build.js
@@ -1,12 +1,30 @@
-require("dotenv").config({ path: ".after-sign.env" });
-
+const { exec } = require("child_process");
 const find = require("lodash/find");
-const compact = require("lodash/compact");
 
 exports.default = async function returnInTuneFiles(context) {
+  const { appOutDir, packager, electronPlatformName } = context;
+  const appVersion = packager.appInfo.productFilename;
+
   const pkgPath = find(context.artifactPaths, (localPath) => localPath.includes(".pkg"));
   const exePath = find(context.artifactPaths, (localPath) => localPath.includes("Setup") && !localPath.includes("blockmap"));
 
-  console.log(compact([pkgPath ? `${__dirname}/${pkgPath}.intunemac` : null, exePath ? `${__dirname}/${exePath.gsub(".exe", "")}.intunemac` : null]));
-  return compact([pkgPath ? `${__dirname}/${pkgPath}.intunemac` : null, exePath ? `${__dirname}/${exePath.gsub(".exe", "")}.intunemac` : null]);
+  console.log("pkgPath", pkgPath);
+  console.log("exePath", exePath);
+
+  if (electronPlatformName === "darwin") {
+    const command = `${appOutDir.replace("dist", "build")}/IntuneAppUtil -c ${pkgPath} -o ${appOutDir}`;
+    console.log("mac command", command);
+    await exec(command);
+
+    return [`${appOutDir}/${pkgPath}.intunemac`];
+  }
+
+  console.log("win command 1", `mkdir ${appOutDir}/intune-source`);
+  await exec(`mkdir ${appOutDir}/intune-source`);
+  console.log("win command 2", `copy /B /Y ${exePath} ${exePath.replace("dist", "dist/intune-source")}`);
+  await exec(`copy /B /Y ${exePath} ${exePath.replace("dist", "dist/intune-source")}`);
+  console.log("win command 3", `${appOutDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${appOutDir}/intune-source" -s "${appOutDir}/Scribe Setup ${appVersion}.exe" -o ${appOutDir}`);
+  await exec(`${appOutDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${appOutDir}/intune-source" -s "${appOutDir}/Scribe Setup ${appVersion}.exe" -o ${appOutDir}`);
+
+  return [`${appOutDir}/${exePath.gsub(".exe", "")}.intunemac`];
 };
