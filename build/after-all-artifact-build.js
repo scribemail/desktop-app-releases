5ad4fc7f947c93b5ee0e27dd62b73b626386c8a7
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-08 07:08:29 +0200

    Update

--- build/after-all-artifact-build.js
+++ build/after-all-artifact-build.js
@@ -3,18 +3,13 @@ const find = require("lodash/find");
 const path = require("path");
 
 exports.default = async function returnInTuneFiles(context) {
-  console.log(context)
   const { outDir, artifactPaths } = context;
 
   const pkgPath = find(artifactPaths, (localPath) => localPath.includes(".pkg"));
   const exePath = find(context.artifactPaths, (localPath) => localPath.includes("Setup") && !localPath.includes("blockmap"));
 
-  console.log("pkgPath", pkgPath);
-  console.log("exePath", exePath);
-
   if (pkgPath) {
     const command = `${outDir.replace("dist", "build")}/IntuneAppUtil -c ${pkgPath} -o ${outDir}`;
-    console.log("mac command", command);
     await exec(command);
 
     return [`${outDir}/${pkgPath}.intunemac`];
@@ -22,12 +17,9 @@ exports.default = async function returnInTuneFiles(context) {
 
   const exeFileName = path.basename(exePath);
 
-  console.log("win command 1", `mkdir ${outDir}/intune-source`);
   await exec(`mkdir ${outDir}/intune-source`);
-  console.log("win command 2", `copy /B /Y ${exePath} ${exePath.replace("dist", "dist/intune-source")}`);
   await exec(`copy /B /Y ${exePath} ${exePath.replace("dist", "dist/intune-source")}`);
-  console.log("win command 3", `${outDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${outDir}/intune-source" -s "${exeFileName}" -o ${outDir}`);
   await exec(`${outDir.replace("dist", "build")}/IntuneWinAppUtil.exe -c "${outDir}/intune-source" -s "${exeFileName}" -o ${outDir}`);
 
-  return [`${outDir}/${exePath.replace(".exe", "")}.intunewin`];
+  return [`${exePath.replace(".exe", "")}.intunewin`];
 };
