822ee785cb2b63f34f334ffa901102e3c3cdc34b
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-28 10:55:52 +0200

    Update

--- build/windows-sign.js
+++ build/windows-sign.js
@@ -4,7 +4,7 @@ exports.default = async function (configuration) {
   const { SSL_USERNAME, SSL_PASSWORD, SSL_TOTP_SECRET } = process.env;
 
   childProcess.execSync(
-    `C:\\projects\\desktop-app\\CodeSignTool-v1.2.0-windows\\CodeSignTool sign -username=${SSL_USERNAME} -password="${SSL_PASSWORD}" --totp_secret="${SSL_TOTP_SECRET}" -input_file_path=${configuration.path} -output_dir_path=${configuration.path}`,
+    `C:\\projects\\desktop-app\\CodeSignTool-v1.2.0-windows\\CodeSignTool.bat sign -username=${SSL_USERNAME} -password="${SSL_PASSWORD}" --totp_secret="${SSL_TOTP_SECRET}" -input_file_path=${configuration.path} -output_dir_path=${configuration.path}`,
     {
       stdio: "inherit"
     }
