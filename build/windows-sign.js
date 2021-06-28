8fb68de6ac6396b77b26b6af2a7a7b806d9adae5
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-28 11:41:18 +0200

    Update

--- build/windows-sign.js
+++ build/windows-sign.js
@@ -1,12 +1,18 @@
 const childProcess = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
-exports.default = async function (configuration) {
+exports.default = function (configuration) {
   const { SSL_USERNAME, SSL_PASSWORD, SSL_TOTP_SECRET, APPVEYOR_BUILD_FOLDER } = process.env;
+  const fileName = path.basename(configuration.path);
+  const filePath = path.dirname(configuration.path);
+
+  childProcess.execSync(`mkdir "${APPVEYOR_BUILD_FOLDER}\\tmp"`, { stdio: "inherit" });
+
+  fs.renameSync(configuration.path, `${APPVEYOR_BUILD_FOLDER}/tmp/${fileName}`);
 
   childProcess.execSync(
-    `cd ${APPVEYOR_BUILD_FOLDER}\\CodeSignTool-v1.2.0-windows && CodeSignTool.bat sign -username=${SSL_USERNAME} -password="${SSL_PASSWORD}" -totp_secret="${SSL_TOTP_SECRET}" -input_file_path=${configuration.path} -output_dir_path=${configuration.path}`,
-    {
-      stdio: "inherit"
-    }
+    `cd ${APPVEYOR_BUILD_FOLDER}\\CodeSignTool-v1.2.0-windows && CodeSignTool.bat sign -username=${SSL_USERNAME} -password="${SSL_PASSWORD}" -totp_secret="${SSL_TOTP_SECRET}" -input_file_path=${APPVEYOR_BUILD_FOLDER}\\tmp\\${fileName} -output_dir_path=${filePath}`,
+    { stdio: "inherit" }
   );
 };
