b754246729b637e56febd9289aa8693cc73cd80d
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-28 11:07:17 +0200

    Update

--- build/windows-sign.js
+++ build/windows-sign.js
@@ -3,6 +3,10 @@ const childProcess = require("child_process");
 exports.default = async function (configuration) {
   const { SSL_USERNAME, SSL_PASSWORD, SSL_TOTP_SECRET, APPVEYOR_BUILD_FOLDER } = process.env;
 
+  childProcess.execSync("dir /s /b /o:gn", {
+    stdio: "inherit"
+  });
+
   childProcess.execSync(
     `${APPVEYOR_BUILD_FOLDER}\\CodeSignTool-v1.2.0-windows\\CodeSignTool.bat sign -username=${SSL_USERNAME} -password="${SSL_PASSWORD}" --totp_secret="${SSL_TOTP_SECRET}" -input_file_path=${configuration.path} -output_dir_path=${configuration.path}`,
     {
