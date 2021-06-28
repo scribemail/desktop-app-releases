bc669e86065af1a341c879b02caad265806692a2
Gil Vandendriessche <gil@scribe-mail.com>
2021-06-28 10:32:21 +0200

    Updat

--- build/windows-sign.js
+++ build/windows-sign.js
@@ -1,3 +1,12 @@
-exports.default = async (configuration) => {
-  console.log(configuration);
+const childProcess = require("child_process");
+
+exports.default = async function (configuration) {
+  const { SSL_USERNAME, SSL_PASSWORD, SSL_TOTP_SECRET } = process.env.SSL_USERNAME;
+
+  childProcess.execSync(
+    `CodeSignTool sign -username=${SSL_USERNAME} -password="${SSL_PASSWORD}" --totp_secret="${SSL_TOTP_SECRET}" -input_file_path=${configuration.path} -output_dir_path=${configuration.path}`,
+    {
+      stdio: "inherit"
+    }
+  );
 };
