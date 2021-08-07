16a627e7a692f7906cd8a856fd46ac9aec17cca9
Gil Vandendriessche <gil@scribe-mail.com>
2021-08-07 20:59:50 +0200

    Add installation recording

--- src/renderer/requests/signature.js
+++ src/renderer/requests/signature.js
@@ -12,7 +12,7 @@ export const getSignatureRawHtml = (id) => (
 
 export const createSignatureInstallation = (id, params) => (
   new Promise((resolve, reject) => {
-    Api.post(`/signatures/${id}/installations}`, params).then((response) => {
+    Api.post(`/signatures/${id}/installation`, { installation: params }).then((response) => {
       resolve(response);
     }).catch((error) => {
       reject(error);
