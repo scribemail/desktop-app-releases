ea9e5b35970ad87c4557d519115bf273bd0dc66a
Gil Vandendriessche <gil@scribe-mail.com>
2021-10-04 20:24:03 +0200

    Update

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -12,7 +12,18 @@ import BugsnagPluginReact   from "@bugsnag/plugin-react";
 import "bootstrap";
 import "renderer/styles/application.scss";
 
+import { i18n }                             from "@lingui/core";
+import { I18nProvider }                     from "@lingui/react";
+import { en as enPlurals, fr as frPlurals } from "make-plural/plurals";
+import { messages as enMessages }           from "locales/en/messages";
+import { messages as frMessages }           from "locales/fr/messages";
+
 import en from "javascript-time-ago/locale/en";
+
+i18n.loadLocaleData("en", { plurals: enPlurals });
+i18n.loadLocaleData("fr", { plurals: frPlurals });
+i18n.load({ en: enMessages, fr: frMessages });
+i18n.activate("en");
 
 TimeAgo.addDefaultLocale(en);
 
@@ -25,9 +36,11 @@ const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
 
 const RootComponent = () => (
   <ErrorBoundary>
-    <SessionProvider>
-      <ApplicationContainer />
-    </SessionProvider>
+    <I18nProvider i18n={ i18n }>
+      <SessionProvider>
+        <ApplicationContainer />
+      </SessionProvider>
+    </I18nProvider>
   </ErrorBoundary>
 );
 
